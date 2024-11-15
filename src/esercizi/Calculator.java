dfb091ac6c55993e0a211374880822011a534bad
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2024-11-16 00:28:18 +0100

    Used methods to show the result of calculations

--- src/esercizi/Calculator.java
+++ src/esercizi/Calculator.java
@@ -13,41 +13,44 @@ public class Calculator {
 		Scanner in = new Scanner(System.in);
 
 		// Leggo e salvo il primo valore
+		System.out.println("Inserisci il primo numero intero: (premi enter per confermare)");
 		int num1 = in.nextInt();
 		System.out.println("Primo numero: " + num1);
 
 		// Leggo e salvo il secondo valore
+		System.out.println("Inserisci il secondo numero intero: (premi enter per confermare)");
 		int num2 = in.nextInt();
 		System.out.println("Secondo numero: " + num2);
 
 		// Chiudo lo scanner
 		in.close();
 
-		System.out.println("Somma: " + (num1 + num2));
-		System.out.println("Sottrazione: " + (num1 - num2));
-		System.out.println("Moltiplicazione: " + (num1 * num2));
-		System.out.println("Divisione: " + (num1 / num2));
-		System.out.println("Resto: " + (num1 % num2));
+		System.out.println("Somma: " + sum(num1, num2));
+		System.out.println("Sottrazione: " + sub(num1, num2));
+		System.out.println("Moltiplicazione: " + mult(num1, num2));
+		System.out.println("Divisione: " + div(num1, num2));
+		System.out.println("Resto: " + mod(num1, num2));
 
 	}
+	
 
-	public int sum(int num1, int num2) {
+	public static int sum(int num1, int num2) {
 		return num1 + num2;
 	}
 	
-	public int sub(int num1, int num2) {
+	public static int sub(int num1, int num2) {
 		return num1 - num2;
 	}
 	
-	public int mult(int num1, int num2) {
+	public static int mult(int num1, int num2) {
 		return num1 * num2;
 	}
 	
-	public int div(int num1, int num2) {
+	public static int div(int num1, int num2) {
 		return num1 / num2;
 	}
 	
-	public int mod(int num1, int num2) {
+	public static int mod(int num1, int num2) {
 		return num1 % num2;
 	}
 
